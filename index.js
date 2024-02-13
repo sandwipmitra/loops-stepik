d556c15f6c83174853628e47a313783527734c87
sandwipmitra <43679584+sandwipmitra@users.noreply.github.com>
2024-02-13 11:08:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+for (i=0; i<=50; i++){
+  if(i==5){
+    continue; //break
+  }
+  console.log(i);
+}
+
+
+// label_1: 
+// for (i = 1; i <= 50; i ++) {
+//   while (i < a) {
+//     console.log (i);
+//     if (i == a) {
+//       break label_1;
+//     }
+
+//     if (i == b) {
+//       continue label_1;
+//     }
+//   }
+// }
